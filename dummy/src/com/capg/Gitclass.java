03831746ed05979624f57a7108b47f6101c8cc15
kkeertha <kkeertha@DIN66007226.corp.capgemini.com>
2018-07-06 17:32:09 +0530

    This is a dummy code

---
+++ dummy/src/com/capg/Gitclass.java
@@ -0,0 +1,11 @@
+package com.capg;
+
+public class Gitclass {
+
+	public static void main(String[] args) {
+		System.out.println("hai welcome to github");
+		System.out.println("enter the credentials");
+	}
+	}
+
+
